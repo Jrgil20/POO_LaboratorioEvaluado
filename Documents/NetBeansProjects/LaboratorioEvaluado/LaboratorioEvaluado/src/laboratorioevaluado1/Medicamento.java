fb6a0f0f7fb3e2b91344cd2e9b69e8d70cb64aed
Jesus Rodolfo Gil farias <131034722+Jrgil20ucab@users.noreply.github.com>
2023-11-08 17:21:52 -0400

    Creacion de metodos concretos
    Se agregan los metodos concretos y se remplazan  los abstrabtos

--- Documents/NetBeansProjects/LaboratorioEvaluado/LaboratorioEvaluado/src/laboratorioevaluado1/Medicamento.java
+++ Documents/NetBeansProjects/LaboratorioEvaluado/LaboratorioEvaluado/src/laboratorioevaluado1/Medicamento.java
@@ -4,7 +4,7 @@ package laboratorioevaluado1;
  * @author Jesus Gil y Matias silveira
  */
 
-public abstract class Medicamento 
+public abstract class Medicamento implements Validaciones
 {//clase que define la estructura de los medicamentos   
     
     //Atributos de la clase abstracta Medicamento     
@@ -29,6 +29,77 @@ public abstract class Medicamento
     protected abstract void mostrarCostoYPrecio();
     protected abstract void determinarVencido();    
     protected abstract int comprarMedicamentos();
-    protected abstract double calcularTotalCompra(int nComprar);
-    protected abstract int devolverCompra(int nComprar);   
+    
+    
+    /////////////////////////METODOS GET Y SET/////////////////////////////////////
+
+    public String getNombreMedicamento() {
+        return nombreMedicamento;
+    }
+    public void setNombreMedicamento(String nombreMedicamento) {
+        this.nombreMedicamento = nombreMedicamento;
+    }
+    public int getCodigoMedicamento() {
+        return codigoMedicamento;
+    }
+    public void setCodigoMedicamento(int codigoMedicamento) {
+        this.codigoMedicamento = codigoMedicamento;
+    }
+    public double getCostoMedicamento() {
+        return costoMedicamento;
+    }
+    public void setCostoMedicamento(double costoMedicamento) {
+        this.costoMedicamento = costoMedicamento;
+    }
+    public double getPrecioVentaPublica() {
+        return precioVentaPublica;
+    }
+    public void setPrecioVentaPublica(double precioVentaPublica) {
+        this.precioVentaPublica = precioVentaPublica;
+    }
+    public int getUnidadesVendidas() {
+        return unidadesVendidas;
+    }
+    public void setUnidadesVendidas(int unidadesVendidas) {
+        this.unidadesVendidas = unidadesVendidas;
+    }
+    public int getUnidadesExistentes() {
+        return unidadesExistentes;
+    }
+    public void setUnidadesExistentes(int unidadesExistentes) {
+        this.unidadesExistentes = unidadesExistentes;
+    }
+    public String getFechaCaducidad() {
+        return fechaCaducidad;
+    }
+    public void setFechaCaducidad(String fechaCaducidad) {
+        this.fechaCaducidad = fechaCaducidad;
+    }
+    public int getVigenciaMercado() {
+        return vigenciaMercado;
+    }
+    public void setVigenciaMercado(int vigenciaMercado) {
+        this.vigenciaMercado = vigenciaMercado;
+    }
+    public int getNumeroLoteMedicamento() {
+        return numeroLoteMedicamento;
+    }
+    public void setNumeroLoteMedicamento(int numeroLoteMedicamento) {
+        this.numeroLoteMedicamento = numeroLoteMedicamento;
+    }
+
+    /////////////////////////METODOS CONCRETOS/////////////////////////////////////
+    protected double calcularTotalCompra(int nComprar)
+    {   // Metodo que calcula el total de la compra de un medicamento
+        return nComprar*precioVentaPublica;
+    }
+
+    protected int devolverCompra(int nComprar)
+    { // Metodo que devuelve la compra de un medicamento
+        nComprar = validarValorNumerico("numero de unidades "
+                + "a devolver",1,nComprar);
+        this.setUnidadesExistentes(unidadesExistentes+nComprar);
+        this.setUnidadesVendidas(unidadesVendidas-nComprar);
+        return nComprar;
+    }    
 }
