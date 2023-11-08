72f795fa5f4cd960e294246137e5d67259cac1cd
Jesus Rodolfo Gil farias <131034722+Jrgil20ucab@users.noreply.github.com>
2023-11-08 21:41:36 +0000

    creacion de metodos concretos getters y setters

--- Documents/NetBeansProjects/LaboratorioEvaluado/LaboratorioEvaluado/src/laboratorioevaluado1/Medicamento.java
+++ Documents/NetBeansProjects/LaboratorioEvaluado/LaboratorioEvaluado/src/laboratorioevaluado1/Medicamento.java
@@ -18,7 +18,8 @@ public abstract class Medicamento implements Validaciones
     protected int vigenciaMercado;//
     protected int numeroLoteMedicamento;//
            
-    //metodos abstractos
+/////////////////////////METODOS ABSTRACTOS/////////////////////////////////////
+
     protected abstract void leerDatos();
     protected abstract double calcularPrecioFinal(int porcentajeAdicional);
     protected abstract void mostrarInformacion();
@@ -31,7 +32,7 @@ public abstract class Medicamento implements Validaciones
     protected abstract int comprarMedicamentos();
     
     
-    /////////////////////////METODOS GET Y SET/////////////////////////////////////
+    /////////////////////////METODOS CONCRETOS GET Y SET/////////////////////////////////////
 
     public String getNombreMedicamento() {
         return nombreMedicamento;
@@ -39,48 +40,56 @@ public abstract class Medicamento implements Validaciones
     public void setNombreMedicamento(String nombreMedicamento) {
         this.nombreMedicamento = nombreMedicamento;
     }
+
     public int getCodigoMedicamento() {
         return codigoMedicamento;
     }
     public void setCodigoMedicamento(int codigoMedicamento) {
         this.codigoMedicamento = codigoMedicamento;
     }
+
     public double getCostoMedicamento() {
         return costoMedicamento;
     }
     public void setCostoMedicamento(double costoMedicamento) {
         this.costoMedicamento = costoMedicamento;
     }
+
     public double getPrecioVentaPublica() {
         return precioVentaPublica;
     }
     public void setPrecioVentaPublica(double precioVentaPublica) {
         this.precioVentaPublica = precioVentaPublica;
     }
+
     public int getUnidadesVendidas() {
         return unidadesVendidas;
     }
     public void setUnidadesVendidas(int unidadesVendidas) {
         this.unidadesVendidas = unidadesVendidas;
     }
+
     public int getUnidadesExistentes() {
         return unidadesExistentes;
     }
     public void setUnidadesExistentes(int unidadesExistentes) {
         this.unidadesExistentes = unidadesExistentes;
     }
+
     public String getFechaCaducidad() {
         return fechaCaducidad;
     }
     public void setFechaCaducidad(String fechaCaducidad) {
         this.fechaCaducidad = fechaCaducidad;
     }
+
     public int getVigenciaMercado() {
         return vigenciaMercado;
     }
     public void setVigenciaMercado(int vigenciaMercado) {
         this.vigenciaMercado = vigenciaMercado;
     }
+    
     public int getNumeroLoteMedicamento() {
         return numeroLoteMedicamento;
     }
